9b56b9984509d794468a00680ae57a11f56b8465
Teresiah6 <wangariteresiah6@gmail.com>
2023-01-24 05:55:53 -0800

    selection list revision

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,22 +1,22 @@
 #include "sort.h"
-/**                                                              
- * swap - Function to swap nodes                                 
- * @list: Double linked list                                     
- * @back: Pointer to the struct to reference the back node       
- * @front: Pointer to the struct to reference the front node     
- */                                                              
+/**
+ * swap - Function to swap nodes
+ * @list: Double linked list
+ * @back: Pointer to the struct to reference the back node
+ * @front: Pointer to the struct to reference the front node
+ */
 void swap(listint_t **list, listint_t *back, listint_t *front)   
-{                                                                
-        if (back->prev)                                          
-                back->prev->next = front;                        
-        else                                                     
-                *list = front;                                   
-        back->next = front->next;                                
-        front->next = back;                                      
-        if (back->next)                                          
-                back->next->prev = back;                         
-        front->prev = back->prev;                                
-        back->prev = front;                                      
+{
+	if (back->prev)
+                back->prev->next = front;
+        else
+		*list = front;
+	back->next = front->next;
+        front->next = back;
+        if (back->next)
+                back->next->prev = back;
+        front->prev = back->prev;
+        back->prev = front;
 }
 /**
  * insertion_sort_list - Function to implement the Insertion Sort Algorithm
@@ -53,4 +53,4 @@ void insertion_sort_list(listint_t **list)
 			}
 		}
 	}
-}	
+}
